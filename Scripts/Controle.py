c07586b1a7162872992e96ea760e6f3cfdbd9890
JoaoRicardoMarques <joaoricardomarques@alunos.utfpr.edu.br>
2024-11-27 00:14:38 -0300

    commit 20

--- Scripts/Controle.py
+++ Scripts/Controle.py
@@ -4,27 +4,28 @@ from Contract_Table import contract
 from Opcodes_Table import opcodes
 from ContractHaveOpcodes_Table import contracts_have_opcodes
 
-duckPath = "/home/joao/Ethereum Data Analyse/Scripts/Teste.db"
 initial = 50000
 final = initial + 9999
+duckPath = f"~/Ethereum Data Analyse/Database/database-{initial}-{initial+49999}.db"
 
 blocks.create(duckPath)
 transactions.create(duckPath)
-contract.create(duckPath)
-opcodes.create(duckPath)
-contracts_have_opcodes.create(duckPath)
+#contract.create(duckPath)
+#opcodes.create(duckPath)
+#contracts_have_opcodes.create(duckPath)
 
 for i in range(5):
-    blocks.insert(duckPath,blockPath)
-    transactions.insert(duckPath,transactionPath)
-    contract.insert(duckPath,contractPath)
-    opcodes.insert(duckPath)
-    contracts_have_opcodes.insert(duckPath)
-
     blockPath=f"/home/joao/Ethereum Data Analyse/Data/Blocks/blocks-00000{initial}-00000{final}.csv"
     transactionPath=f"/home/joao/Ethereum Data Analyse/Data/Transactions/transactions-of-blocks-00000{initial}-00000{final}.csv"
-    contractPath=""
 
+    blocks.insert(duckPath,blockPath)
+    transactions.insert(duckPath,transactionPath)
+    #contract.insert(duckPath,contractPath)
+    #opcodes.insert(duckPath)
+    #contracts_have_opcodes.insert(duckPath)
+    
+    initial+=10000
+    final+=10000
 
 
 
